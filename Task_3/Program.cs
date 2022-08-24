29d459896b1e4cc0bf6e7ce377a8f3baf3852702
PolinaG <PolinaG@gmail.com>
2022-08-24 16:22:13 +0300

    Create a task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] arr = new string[7] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+Console.Write("Введите число от 1 до 7: ");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number > 5 && number <= 7)
+{
+Console.WriteLine($"{number} день недели это - {arr[number - 1]} выходной день");
+}
+
+else if (number > 7 || number < 1)
+{
+Console.WriteLine("Неверное число.");
+}
+else
+{
+Console.WriteLine($"{number} день недели это - {arr[number - 1]} не выходной день");
+}
