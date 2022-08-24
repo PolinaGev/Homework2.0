75d7731ea52aeb72e485a11ecbe9c09d386f4c1e
PolinaG <PolinaG@gmail.com>
2022-08-24 15:25:05 +0300

    Create a task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+while (number >= 100)
+        {
+            number /= 10;
+        }
+
+        int result = number % 10;
+
+System.Console.WriteLine($"Второе число = {result}");
