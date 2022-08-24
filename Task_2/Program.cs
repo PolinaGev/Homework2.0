8e69d28346ac2f69ff7c8b764d45e7308f9b73bc
PolinaG <PolinaG@gmail.com>
2022-08-24 16:01:26 +0300

    Create a task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number > 100 && number < 1000)
+        {
+            number = number % 10;
+            System.Console.WriteLine($"Третье число = {number}");
+        }
+else{
+    Console.WriteLine($"Число {number} не трехзначное");
+}
+
+
